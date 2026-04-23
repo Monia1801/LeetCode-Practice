818ce1487bc018e0a052e54386da74a6fe271db0
Monia1801 <moniavoona18@gmail.com>
2026-04-23 14:35:30 +0530

    Time: 1936 ms (5.3%), Space: 152.6 MB (25.44%) - LeetHub

---
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& nums) {
+        vector<long long> res(nums.size());
+        map<int,vector<int>> mp;
+        for(int i=0;i<nums.size();i++) mp[nums[i]].push_back(i);
+        for(auto i:mp){
+            cout<<i.first<<":";
+            for(auto j:i.second) cout<<j<<" ";
+            cout<<endl;
+        }
+        //CORRECT BUT GIVES TLE
+        // for(int i=0;i<nums.size();i++){
+        //     int sum=0;
+        //     for(auto j:mp[nums[i]]) sum+=abs(j-i);
+        //     res[i]=sum;
+        // }
+
+        for(auto i:mp){
+            vector<int>a=i.second;
+            int n=a.size();
+            long long total=0;
+            for(auto k:a) total+=k;
+
+            long long prefix=0;
+            for(int j=0;j<n;j++){
+                long long left=1LL*j*a[j]-prefix;
+                long long right=(total-prefix-a[j])-1LL*(n-j-1)*a[j];  
+
+                res[a[j]]=left+right;
+                prefix+=a[j];
+            }
+        }
+        return res;
+    }
+};
