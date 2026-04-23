76eb7179f8ec218119817dfa713980fb2526543b
Monia1801 <moniavoona18@gmail.com>
2026-04-23 11:45:43 +0530

    Time: 1241 ms (5%), Space: 128.6 MB (19.25%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -19,7 +19,8 @@ public:
         for(auto i:mp){
             vector<int>a=i.second;
             int n=a.size();
-            long long total=accumulate(a.begin(),a.end(),0LL);
+            long long total=0;
+            for(auto k:a) total+=k;
 
             long long prefix=0;
             for(int j=0;j<n;j++){
