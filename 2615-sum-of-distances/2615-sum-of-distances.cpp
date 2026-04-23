c189b5ebe315860ffec3087b797b9b509b79cd34
Monia1801 <moniavoona18@gmail.com>
2026-04-23 11:45:00 +0530

    Time: 1227 ms (5%), Space: 128.6 MB (19.25%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        vector<long long> res(nums.size());
+        map<int,vector<int>> mp;
+        for(int i=0;i<nums.size();i++) mp[nums[i]].push_back(i);
+        for(auto i:mp){
+            cout<<i.first<<":";
+            for(auto j:i.second) cout<<j<<" ";
+            cout<<endl;
+        }
+        //CORRECT BUT GIVES TLE
+        // for(int i=0;i<nums.size();i++){
+        //     int sum=0;
+        //     for(auto j:mp[nums[i]]) sum+=abs(j-i);
+        //     res[i]=sum;
+        // }
+
+        for(auto i:mp){
+            vector<int>a=i.second;
+            int n=a.size();
+            long long total=accumulate(a.begin(),a.end(),0LL);
+
+            long long prefix=0;
+            for(int j=0;j<n;j++){
+                long long left=1LL*j*a[j]-prefix;
+                long long right=(total-prefix-a[j])-1LL*(n-j-1)*a[j];  
+
+                res[a[j]]=left+right;
+                prefix+=a[j];
+            }
+        }
+        return res;
+    }
+};
