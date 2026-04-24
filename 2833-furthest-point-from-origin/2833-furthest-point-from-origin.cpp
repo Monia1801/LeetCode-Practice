7de76679ad2cdf827601cff9e1f8cdd89b3873c5
Monia1801 <moniavoona18@gmail.com>
2026-04-24 14:16:29 +0530

    Time: 0 ms (100%), Space: 8.9 MB (90.95%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -6,31 +6,32 @@ public:
             if(i=='L') cntL++;
             else if(i=='R') cntR++;
         }
-        int pnt=0;
         int cntU=moves.length()-cntL-cntR;
-        if(cntL==cntR){
-            for(auto i:moves){
-                if(i=='L') pnt--;
-                else pnt++;
-            }
-            return pnt;
-        }
-        else{
-            if(cntL>cntR){
-                for(auto i:moves){
-                    if(i=='R') pnt++;
-                    else pnt--;
-                }
-                return abs(pnt);
-            }
-            else{
-                for(auto i:moves){
-                    if(i=='L') pnt--;
-                    else pnt ++;
-                }
-                return pnt;
-            }
-        }
-        return 0;
+        return abs(cntL-cntR)+cntU;
+        // int pnt=0;
+        // if(cntL==cntR){
+        //     for(auto i:moves){
+        //         if(i=='L') pnt--;
+        //         else pnt++;
+        //     }
+        //     return pnt;
+        // }
+        // else{
+        //     if(cntL>cntR){
+        //         for(auto i:moves){
+        //             if(i=='R') pnt++;
+        //             else pnt--;
+        //         }
+        //         return abs(pnt);
+        //     }
+        //     else{
+        //         for(auto i:moves){
+        //             if(i=='L') pnt--;
+        //             else pnt ++;
+        //         }
+        //         return pnt;
+        //     }
+        // }
+        // return 0;
     }
 };
