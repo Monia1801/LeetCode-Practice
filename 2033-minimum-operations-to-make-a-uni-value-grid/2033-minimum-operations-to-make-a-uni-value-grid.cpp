24d8203ee26a2035741c440d058fe7e5d53d1f67
Monia1801 <moniavoona18@gmail.com>
2026-04-28 11:10:23 +0530

    Time: 40 ms (54.73%), Space: 90.2 MB (18.52%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> nums;
+
+        for (auto &row : grid) {
+            for (int val : row) {
+                nums.push_back(val);
+            }
+        }
+
+        int rem = nums[0] % x;
+
+        for (int val : nums) {
+            if (val % x != rem) return -1;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        int med = nums[nums.size() / 2];
+        int ops = 0;
+
+        for (int val : nums) {
+            ops += abs(val - med) / x;
+        }
+
+        return ops;
+    }
+};
