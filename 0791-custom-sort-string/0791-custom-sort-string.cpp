f5b95197d6c4b404fd9d49e0748fd03fc529eb48
Monia1801 <moniavoona18@gmail.com>
2026-04-29 20:34:10 +0530

    Time: 0 ms (100%), Space: 8.2 MB (88.66%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> freq(26, 0);
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string result = "";
+        for (char c : order) {
+            while (freq[c - 'a']-- > 0) {
+                result += c;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            while (freq[i]-- > 0) {
+                result += (char)(i + 'a');
+            }
+        }
+
+        return result;
+    }
+};
