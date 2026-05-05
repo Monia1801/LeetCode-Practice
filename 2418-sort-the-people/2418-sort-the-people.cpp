c89309ba1b6e293ba115def6cc1b4735c8aa2998
Monia1801 <moniavoona18@gmail.com>
2026-05-05 18:51:59 +0530

    Time: 11 ms (59.03%), Space: 28.6 MB (46.42%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int,string> mp;
+        for(int i=0;i<names.size();i++) mp[heights[i]]=names[i];
+        vector<string> res;
+        for(auto it=mp.rbegin();it!=mp.rend();it++) res.push_back(it->second);
+
+        return res;
+    }
+};
