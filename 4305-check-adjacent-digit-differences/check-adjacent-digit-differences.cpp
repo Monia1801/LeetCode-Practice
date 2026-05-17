7a1319adc6a091f134971ec9e69cb0b2e9885d10
Monia1801 <moniavoona18@gmail.com>
2026-05-17 12:56:33 +0530

    Time: 39 ms (0.64%) | Memory: 8.6 MB (48.35%) - LeetSync

---
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        for(int i=0;i<s.length()-1;i++){
+            int diff=abs((s[i]-'0')-(s[i+1]-'0'));
+            cout<<diff<<endl;
+            if(diff>2) return false;
+        }
+        return true;
+    }
+};
