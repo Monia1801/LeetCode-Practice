f3702cc72612ec5c1ace58dc625c250d89e3252f
Monia1801 <moniavoona18@gmail.com>
2026-05-26 13:49:47 +0530

    Time: 561 ms (56.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT name AS Customers
+FROM Customers
+WHERE NOT EXISTS(
+    SELECT customerId
+    FROM Orders
+    WHERE Customers.id=Orders.customerId
+);
