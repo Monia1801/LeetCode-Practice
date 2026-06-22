748466df13933a1368b22c33719a156cc2e8b853
Monia1801 <moniavoona18@gmail.com>
2026-06-22 22:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (95.38%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -9,16 +9,18 @@ public:
             else if(i=='o') o++;
             else if(i=='n') n++;
         }
-        int cnt=0;
-        while(b>0 && a>0 && l>0 && o>0 && n>0){
-            b-=1;
-            a-=1;
-            n-=1;
-            if(l<2 || o<2) return cnt;
-            if(l>1) l-=2;
-            if(o>1) o-=2;
-            cnt++;
-        }
-        return cnt;
+        // int cnt=0;
+        // while(b>0 && a>0 && l>0 && o>0 && n>0){
+        //     b-=1;
+        //     a-=1;
+        //     n-=1;
+        //     if(l<2 || o<2) return cnt;
+        //     if(l>1) l-=2;
+        //     if(o>1) o-=2;
+        //     cnt++;
+        // }
+        // return cnt;
+
+        return min({b,a,l/2,o/2,n});
     }
 };
