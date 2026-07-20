6afbe06fc888483d01c8bf455d3cc50c577aa566
Monia1801 <moniavoona18@gmail.com>
2026-07-20 18:54:12 +0530

    Time: 1 ms (54.88%) | Memory: 18.1 MB (91.78%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        k %= (m * n);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int curr = i * n + j;
+                int next = (curr + k) % (m * n);
+
+                ans[next / n][next % n] = grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
