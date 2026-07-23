45cbfef55b1fe4eb4f8c33dc76c59d947fd92880
Monia1801 <moniavoona18@gmail.com>
2026-07-23 09:49:17 +0530

    Time: 8 ms (7.01%) | Memory: 9.7 MB (76.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -9,10 +9,16 @@ public:
         // }
         // return true;
 
-        unordered_map<int,int> mp;
-        for(auto i:s) mp[i]++;
-        for(auto i:t) mp[i]--;
-        for(auto i:mp) if(i.second!=0) return false;
+        // unordered_map<int,int> mp;
+        // for(auto i:s) mp[i]++;
+        // for(auto i:t) mp[i]--;
+        // for(auto i:mp) if(i.second!=0) return false;
+        // return true;
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++) if(s[i]!=t[i]) return false;
         return true;
     }
 };
