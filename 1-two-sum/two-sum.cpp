4fef549bbdce91b3997610a5b46bfc69a6bc7ad9
Monia1801 <moniavoona18@gmail.com>
2026-08-05 20:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (22.83%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& vec, int target) {
+    //----------------SOLUTION 1 --------------------
+    // unordered_map<int,int> cnt;
+    // int j=0;
+    // for(auto i:vec){
+    //     cnt[i]=j;
+    //     j++;
+    // }
+    // vector<int> ans;
+    // for(int i=0;i<vec.size();i++){
+    //     if(cnt.find(target-vec[i])!=cnt.end() && i != cnt[target-vec[i]]){
+    //         ans.push_back(i);
+    //         ans.push_back(cnt[target-vec[i]]);
+    //         break;
+    //     }
+    // }
+    // return ans;
+
+//------------------SOLUTION 2---------------------
+//     vector<pair<int,int>> p;
+
+//     for(int i=0;i<vec.size();i++){
+//         p.push_back({vec[i],i});
+//     }
+
+//     sort(p.begin(),p.end());
+
+//     vector<int> ans;
+//     int left=0,right=vec.size()-1;
+
+//     while(left!=right){
+//         int curr=p[left].first+ p[right].first;
+//         if(curr==target){
+//             ans.push_back(p[left].second);
+//             ans.push_back(p[right].second);
+//             break;
+//         }
+//         else if(curr<target) left++;
+//         else right--;
+//     }
+//     return ans;
+
+
+//-----------------solution 3-----------------
+    unordered_map<int,int> mp;
+    for(int i=0;i<vec.size();i++){
+        int req=target-vec[i];
+        if(mp.find(req)!=mp.end()) return {mp[req],i};
+        mp[vec[i]]=i;
+        }
+    return {};
+    }
+};
